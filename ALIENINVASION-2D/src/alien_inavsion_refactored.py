d32dd9303f50c9022854a5fa64fa5693c08740d3
pedrosferreira-cyber <pedro.s.ferreira@aln.senaicimatec.edu.br>
2026-05-19 07:42:44 -0300

    refatoração do código: aplicando o princípio O

--- ALIENINVASION-2D/src/alien_inavsion_refactored.py
+++ ALIENINVASION-2D/src/alien_inavsion_refactored.py
@@ -10,3 +10,23 @@ from game_renderer import GameRenderer
 
 class AlienInvasion:
     """Gerencia o jogo e seus comportamentos."""
+
+    def __init__(self):
+        """Construtor da classe que inicializa o jogo e cria os recursos básicos"""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien Invasion")
+
+        # Criando uma instância da classe Ship para representar a nave espacial
+        self.ship = Ship(self.screen, self.settings)
+
+        # Mudando a cor do plano de fundo em RGB
+        self.bg_color = self.settings.bg_color
+
+        self.bullet_manager = BulletManager(self.screen, self.settings, self.ship)
+        self.fleet_manager = FleetManager(self.screen, self.settings, self.ship, FastAlien)
+        self.event_handler = GameEventHandler(self.ship, self.bullet_manager)
